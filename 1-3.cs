a8276c3fd92748b87f3af626886d21e76c7c969b
Ali <alitaqizade2@gmail.com>
2023-03-05 20:23:40 +0400

    first commit

---
+++ 1-3.cs
@@ -0,0 +1,23 @@
+﻿//First task
+//int yearOfBirth = 2010;
+
+//if (!((yearOfBirth+=18) > 2013))
+//{
+//    Console.WriteLine("18 yashiniz tamam olub");
+//}
+//else
+//{
+//    Console.WriteLine("18 yashiniz tamam olmayib");
+//}
+
+
+
+//Third task
+//int number = 3494;
+
+//int a = number % 10;
+//int b = number / 10 % 10;
+//int c = number / 100 % 10;
+//int d = number / 1000 % 10;
+
+//Console.WriteLine(a + b + c + d);
